42a67a522b27facc4fdf79e3b47a00f3e442442c
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2023-01-18 15:37:22 +0700

    update

--- header.php
+++ header.php
@@ -8,14 +8,15 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-   
+
     <?php wp_head(); ?>
 </head>
 
 <body>
     <header class="c-header">
         <div class="l-container">
-            <h1 class="c-logo"><a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"" alt="Allgrow Labo"></a></h1>
+            <h1 class="c-logo"><a href="<?php echo get_site_url(); ?>"><img
+                        src="<?php echo get_template_directory_uri(); ?>/img/logo.png"" alt=" Allgrow Labo"></a></h1>
             <?php task14_menu('primary'); ?>
         </div>
     </header><!-- /header -->
